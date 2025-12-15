c645177f0cb69230294f5e3466b3be0898896ea6
scarecrowlee2 <scarecrowlee2@gmail.com>
2025-12-15 09:40:29 +0900

    Add files via upload

---
+++ src/main/java/com/example/cafeProject/communityBoardComment/NoticeBoardCommentDTO.java
@@ -0,0 +1,16 @@
+package com.example.cafeProject.noticeBoardComment;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+public class NoticeBoardCommentDTO {
+    private int id;
+    private String content;
+    private Timestamp createDate;
+    private int memberId;
+    private int noticeBoardId;
+}
